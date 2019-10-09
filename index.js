e786d1fc3ee5381985fef748fc35abdace036dbf
JoeriCaluwe <s094203@ap.be>
2019-10-09 10:58:40 +0200

    no message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var express = require("express");
 var path = require("path");
 
@@ -38,3 +39,49 @@ app.get('/profiel', (req, res) => {
 });
 
 app.listen(3000);
+=======
+var express = require("express");
+var path = require("path");
+
+var content = require("./data/application.json");
+var port = 3000;
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"))
+
+app.get("/", function(request, response) {
+    response.render("home", {
+        boodschap: "online!"
+    });
+app.get('/leaflet', (req, res) => {
+    res.render('leaflet');
+    });
+app.get('/chat', (req, res) => {
+    res.render('chat');
+    });
+app.get('/detailpark', (req, res) => {
+    res.render('detailpark');
+    });
+app.get('/lost', (req, res) => {
+    res.render('lost');
+    });
+app.get('/weer', (req, res) => {
+    res.render('weer');
+    });
+app.get('/parken', (req, res) => {
+    res.render('parken');
+    });
+app.get('/carpool', (req, res) => {
+    res.render('carpool');
+    });
+app.get('/profiel', (req, res) => {
+    res.render('profiel');
+    });
+});
+
+
+app.listen(3000);
+>>>>>>> initial commit;
