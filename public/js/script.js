f69c3f8055ada1b0b7b229c9fe07cb945678a77e
Nicolas De Keersmaeker <nicolas.dekeersmaeker@student.ap.be>
2019-11-07 09:36:51 +0100

    leaflet markers op alle locaties toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,13 @@ var mymap = L.map('mapje', {
 var basicmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	id: 'mapbox.streets'
 });
+
+
+
+
+
 basicmap.addTo(mymap);
+
 
 //deze regels zijn maar een voorbeeld
 /*
