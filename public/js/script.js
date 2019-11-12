4953af6f0ee080633d0f69972334a5cc64bcdc7b
joeri caluwe <s094203@ap.be>
2019-11-12 12:21:52 +0100

    -huidige locatie weergeven -knop om terug te keren naar de home -placeholder slidemenu toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,20 @@
 var mymap = L.map('mapje', {
-  center: [51.230253, 4.416149],
-  zoom: 14
 });
 
 var basicmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	id: 'mapbox.streets'
 });
 
-
-
+function onLocationFound(e) {
+var radius = e.accuracy / 2;
+L.marker(e.latlng).addTo(mymap) .bindPopup("You are Here").openPopup();
+L.circle(e.latlng, radius).addTo(mymap);
+}
+mymap.on('locationfound', onLocationFound);
+mymap.locate({setView: true, watch: true, maxZoom: 15});
 
 
 basicmap.addTo(mymap);
 
 
-//deze regels zijn maar een voorbeeld
-/*
-var school = L.icon({
-    iconUrl: 'school.png',
-    iconSize: [75, 95],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76],});
-
-var kerk = L.icon({
-    iconUrl: 'kerk.png',
-    iconSize: [75, 95],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76]});
-
-L.marker([51.230253, 4.416149], {icon:school}).addTo(mymap);
-L.marker([51.2195765,4.4018782], {icon:kerk}).addTo(mymap);
-L.circle([51.213524, 4.417204], {color:'#ff0000', radius: 3000}).addTo(mymap);
-*/
+//---------slide menu--------
