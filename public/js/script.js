83ea69ea2e175b999e5462fb227f570080786ef2
Eva Willemsen <eva.willemsen@student.ap.be>
2019-11-28 10:00:16 +0100

    Aparte afbeelding voor eigen locatie

--- public/js/script.js
+++ public/js/script.js
@@ -4,10 +4,19 @@ var mymap = L.map('mapje', {
 var basicmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	id: 'mapbox.streets'
 });
+var myLocationPin = L.icon({
+	iconUrl: 'css/images/mama.png',
+    shadowUrl: 'css/images/mama.png',
 
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 function onLocationFound(e) {
 var radius = e.accuracy / 2;
-L.marker(e.latlng).addTo(mymap) .bindPopup("You are Here").openPopup();
+L.marker(e.latlng,{icon:myLocationPin}).addTo(mymap) .bindPopup("You are Here").openPopup();
 L.circle(e.latlng, radius).addTo(mymap);
 }
 mymap.on('locationfound', onLocationFound);
